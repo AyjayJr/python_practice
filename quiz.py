d7ca6f67bf47e008e6b56c8d89baf2529336a8a4
AyjayJr <ayjayjr@hotmail.com>
2021-09-12 01:31:26 -0400

    5 question quiz on hardware acronyms

---
+++ quiz.py
@@ -0,0 +1,51 @@
+print("Welcome to my quiz!")
+ans = input("Would you like to play? (y/n): ")
+accept = ("yeah", "yes", "yea", "y")
+decline = ("never", "nah", "no", "n")
+
+while ans not in accept and ans not in decline:
+    ans = input("Please make a valid selection. (y/n): ")
+if ans in decline:
+    print("bye!")
+    quit()
+
+print("Okay! Let's play.")
+score = 0;
+
+ans = input("What does CPU stand for? ")
+if ans.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+ans = input("What does GPU stand for? ")
+if ans.lower() == "graphics processing unit": 
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+ans = input("What does RAM stand for? ")
+if ans.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+ans = input("What does HDD stand for? ")
+if ans.lower() == "hard disk drive":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+ans = input("What does SSD stand for? ")
+if ans.lower() == "solid state drive":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+    
+print("Your score is {}/5".format(score))
+print("See you next time")
