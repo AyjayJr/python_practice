f9268e27a2548b1f931df211f9978113970f88fd
AyjayJr <ayjayjr@hotmail.com>
2021-12-28 22:11:05 -0500

    selection sort

--- dsa/algorithms/selection_sort.py
+++ dsa/algorithms/selection_sort.py
@@ -1,20 +1,21 @@
 def selection_sort(lst):
     
-    for i in range(len(lst)):
+    for i in range(len(lst) - 1):
         min = i
-        for j in range(i, len(lst)):
-            if lst[j] < lst[i]:
+        for j in range(i + 1, len(lst)):
+            if lst[j] < lst[min]:
                 min = j
-        temp = lst[i]
-        lst[i] = lst[min]
-        lst[min] = temp
+        if i != min:
+            temp = lst[i]
+            lst[i] = lst[min]
+            lst[min] = temp
+    return lst
 
 
 def main():
     lst = [5,3,6,1,7,3]
     print(lst)
-    selection_sort(lst)
-    print(lst)
+    print(selection_sort(lst))
 
 if __name__ == "__main__":
     main()
